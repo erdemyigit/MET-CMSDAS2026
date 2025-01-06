be49d0611e26f0da26f40d1d044a8b765bb559ed
Garvita <garvitaa@buffalo.edu>
2025-01-06 08:55:06 -0600

    CMSDAS short-excercise for MET

---
+++ CMSDAS_MET_Analysis/test/run_CMSDAS_MET_Exercise2_cfg.py
@@ -0,0 +1,31 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("CMSDASMET")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load("Configuration.EventContent.EventContent_cff")
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.MessageLogger.cerr.FwkReport.reportEvery = 10000
+
+
+process.TFileService = cms.Service("TFileService", fileName = cms.string("./outputs/cmsdas_met_exercise2.root") )
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+process.source = cms.Source(
+    "PoolSource",
+    fileNames = cms.untracked.vstring([
+            'root://cmsxrootd.fnal.gov//store/user/cmsdas/2025/short_exercises/MET/DYJetsToLL_M50_amcatnloFXFX.root',
+            ]
+                                      )
+    )
+
+process.cmsdasmetexercise2 = cms.EDAnalyzer('CMSDAS_MET_AnalysisExercise2',
+                                    doprints = cms.bool(False),
+                                    mets     = cms.InputTag("slimmedMETs")
+                                    )
+
+process.p = cms.Path(process.cmsdasmetexercise2)
